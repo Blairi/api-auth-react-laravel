d12bc987e20a5e6d5d5dbab0166b73dad879e84b
Blairi <axel.avi.fer@gmail.com>
2022-10-05 12:21:20 -0500

    custom hooks

---
+++ api-auth-react/src/hooks/useCheckAuth.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { validateUserToken } from "../services";
+import { login, logout } from "../store/auth/authSlice";
+
+export const useCheckAuth = () => {
+
+  const { status } = useSelector( state => state.auth );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    
+    (async () => {
+
+      const token = sessionStorage.getItem("token");
+
+      if (!token) return dispatch( logout() );
+
+      const data = await validateUserToken({ token });
+      
+      if (!data.status) return dispatch( logout() ); // Inavalid token
+
+      const payload = {
+        email: data.data.email,
+        displayName: data.data.name,
+        token: token
+      }
+
+      dispatch( login(payload) );
+
+    })()
+
+  }, []);
+
+  return {
+    status
+  }
+
+}
